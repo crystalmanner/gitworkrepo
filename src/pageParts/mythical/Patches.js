e578a95900ac6c9095db268928050913ed6d3d20
root <crystalmanner130@gmail.com>
2013-08-05 16:29:31 -0700

    Patches.js updated

--- src/pageParts/mythical/Patches.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import patches from '../../img/mythical/patches.png'
-
-export default props => (
-  <section className="patches">
-    <img src={patches} />
-  </section>
-)
